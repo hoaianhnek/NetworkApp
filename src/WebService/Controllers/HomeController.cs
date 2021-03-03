f82dd13967792b7ffc877400fa6bf5ab8590ea42
ANHSTRUONGW\hoaia <hoaianhnek@gmail.com>
2021-03-03 16:49:17 +0700

    set up project

---
+++ src/WebService/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using NetworkDataAccess;
+using System.Linq;
+using System.Web.Http;
+
+namespace WebService.Controllers
+{
+    [RoutePrefix("webservice/api")]
+    public class HomeController : ApiController
+    {
+        [HttpGet]
+        [Route("test")]
+        public IHttpActionResult Index()
+        {
+            using(DBNetworkEntities entities = new DBNetworkEntities())
+            {
+                return Ok(entities.tests.ToList());
+            }
+            
+        }
+    }
+}
